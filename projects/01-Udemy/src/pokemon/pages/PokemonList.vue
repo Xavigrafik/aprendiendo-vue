c2616de7fd5b0fd723fe991490a2b589b6c2c601
xavi <xavigrafik@gmail.com>
2026-07-22 10:44:50 +0200

    getPokemons para obtener datos de Pokémon y actualizar PokemonList.vue

--- projects/01-Udemy/src/pokemon/pages/PokemonList.vue
+++ projects/01-Udemy/src/pokemon/pages/PokemonList.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 
     import pokemonApi from "../api/pokemonApi.ts";
+import { getPokemons } from "../helpers/get-pokemons.ts";
     import type { PokemonListResponse } from "../interfaces/pokemon-list.response.ts";
 
-    pokemonApi.get<PokemonListResponse>('/pokemon?limit=45')
-      .then((response) => {
-        console.log(response.data);
-        const firstPokemonName = response.data?.results?.[0]?.name ?? 'N/A';
-        console.log(firstPokemonName);
-      })
-        .catch(error => {
-            console.error('Error fetching Pokemon:', error);
-        });
+    getPokemons().then(data => console.log(data));
+
+    // pokemonApi.get<PokemonListResponse>('/pokemon?limit=45')
+    //   .then((response) => {
+    //     console.log(response.data);
+    //     const firstPokemonName = response.data?.results?.[0]?.name ?? 'N/A';
+    //     console.log(firstPokemonName);
+    //   })
+    //     .catch(error => {
+    //         console.error('Error fetching Pokemon:', error);
+    //     });
 </script>
 
 <template>
